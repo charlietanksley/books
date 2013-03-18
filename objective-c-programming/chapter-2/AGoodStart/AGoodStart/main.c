e21dfa04dee171e94962f0495ed9f87a6077c196
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-03-17 20:44:33 -0400

    chapter 2

---
+++ objective-c-programming/chapter-2/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Charlie Tanksley on 3/17/13.
+//  Copyright (c) 2013 Charlie Tanksley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+       Maybe it needs a rewrite. */
+
+    return 0;
+}
+
