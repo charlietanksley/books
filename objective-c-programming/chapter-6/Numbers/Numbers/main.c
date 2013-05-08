eda8e116b87c42ac320ddbb98bd324bb4eedb66f
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-05-08 10:12:20 -0400

    chapter 6: Numbers

---
+++ objective-c-programming/chapter-6/Numbers/Numbers/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Charlie Tanksley on 5/8/13.
+//  Copyright (c) 2013 Charlie Tanksley. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("%.3f", sin(1));
+
+    return 0;
+}
+
